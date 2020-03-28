7ea1b1d1d83e850f3c983f8a35aac4711fc484b4
Przemyslaw Pomiechowski <p.pomiechowski@outlook.com>
2020-03-28 13:00:06 +0000

    Dodano wyswietlanie listy zadan

--- store.js
+++ store.js
@@ -27,7 +27,22 @@ const createTask = async (title, desc) => (
   })
 )
 
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result, response) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
+
 module.exports = {
   init,
-  createTask
+  createTask,
+  listTasks
 }
